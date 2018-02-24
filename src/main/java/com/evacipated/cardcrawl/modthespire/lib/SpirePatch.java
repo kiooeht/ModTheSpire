33f146e598a05f35d0cbbe16ce92b365358886d2
Kiooeht <kiooeht@gmail.com>
2018-02-23 17:54:07 -0800

    v1.4
    Allow multiple SpirePatches on one class

--- src/main/java/com/evacipated/cardcrawl/modthespire/lib/SpirePatch.java
+++ src/main/java/com/evacipated/cardcrawl/modthespire/lib/SpirePatch.java
@@ -1,10 +1,8 @@
 package com.evacipated.cardcrawl.modthespire.lib;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
+@Repeatable(SpirePatches.class)
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface SpirePatch {
