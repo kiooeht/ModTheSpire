f87dd58d35316404e0df45b794021b35d23201dc
Kiooeht <kiooeht@gmail.com>
2018-03-04 16:53:59 -0800

    Maintain launcher window size

--- src/main/java/com/evacipated/cardcrawl/modthespire/ModSelectWindow.java
+++ src/main/java/com/evacipated/cardcrawl/modthespire/ModSelectWindow.java
@@ -4,26 +4,89 @@ import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.io.File;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.io.*;
+import java.util.Properties;
 
 public class ModSelectWindow extends JFrame {
     /**
      * 
      */
     private static final long serialVersionUID = -8232997068791248057L;
+    private static final String WINDOW_PROPERTIES_FILEPATH = "ModTheSpire.properties";
     private File[] mods;
     private ModInfo[] info;
+    private Properties windowProperties;
+    private int widthOffset = 0;
+    private int heightOffset = 0;
     
     public ModSelectWindow(File[] modJars) {
         mods = modJars;
         info = Loader.buildInfoArray(mods);
+        readWindowPosSize();
         initUI();
+    }
+
+    private void readWindowPosSize()
+    {
+        windowProperties = new Properties();
+        File file = new File(WINDOW_PROPERTIES_FILEPATH);
+        if (file.exists()) {
+            try {
+                windowProperties.load(new FileInputStream(file));
+            } catch (IOException e) {
+            }
+        } else {
+            windowProperties.setProperty("x", "center");
+            windowProperties.setProperty("y", "center");
+            windowProperties.setProperty("width", "300");
+            windowProperties.setProperty("height", "200");
+            saveWindowProperties();
+        }
     }
 
     private void initUI() {
         setTitle("Mod The Spire " + Loader.MTS_VERSION.get());
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(true);
+
+        int w, h;
+        try {
+            w = Integer.parseInt(windowProperties.getProperty("width"));
+        } catch (NumberFormatException e) {
+            w = 300;
+        }
+        try {
+            h = Integer.parseInt(windowProperties.getProperty("height"));
+        } catch (NumberFormatException e) {
+            h = 200;
+        }
+
+        ModSelectWindow tmpthis = this;
+        final int finalW = w;
+        final int finalH = h;
+        this.addComponentListener(new ComponentAdapter()
+        {
+            private boolean firstResize = true;
+
+            @Override
+            public void componentResized(ComponentEvent e)
+            {
+                super.componentResized(e);
+
+                Dimension d = tmpthis.getContentPane().getSize();
+                if (firstResize) {
+                    firstResize = false;
+                    widthOffset = d.width - finalW;
+                    heightOffset = d.height - finalH;
+                } else {
+                    d.width -= widthOffset;
+                    d.height -= heightOffset;
+                    setWindowDimensions(d);
+                }
+            }
+        });
 
         rootPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -35,7 +98,7 @@ public class ModSelectWindow extends JFrame {
         LoadOrder.loadModsInOrder(model, mods, info);
 
         JScrollPane modScroller = new JScrollPane(modList);
-        modScroller.setPreferredSize(new Dimension(300, 200));
+        modScroller.setPreferredSize(new Dimension(w, h));
         this.getContentPane().add(modScroller, BorderLayout.CENTER);
 
         // Play button
@@ -89,5 +152,23 @@ public class ModSelectWindow extends JFrame {
 
         pack();
         setLocationRelativeTo(null);
+    }
+
+    void saveWindowProperties()
+    {
+        try {
+            windowProperties.store(new FileOutputStream(WINDOW_PROPERTIES_FILEPATH), null);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void setWindowDimensions(Dimension d)
+    {
+        windowProperties.setProperty("width", String.valueOf(d.width));
+        windowProperties.setProperty("height", String.valueOf(d.height));
+        saveWindowProperties();
     }
 }
