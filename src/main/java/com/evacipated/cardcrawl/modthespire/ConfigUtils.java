bb1909089ebb52f38603eb1f3f992124295ca219
Kiooeht <kiooeht@gmail.com>
2018-03-06 22:32:49 -0800

    Obtain user data directory

---
+++ src/main/java/com/evacipated/cardcrawl/modthespire/ConfigUtils.java
@@ -0,0 +1,37 @@
+package com.evacipated.cardcrawl.modthespire;
+
+import org.apache.commons.lang3.SystemUtils;
+
+import java.io.File;
+
+public class ConfigUtils
+{
+    private static final String APP_NAME = "ModTheSpire";
+    public static final String CONFIG_DIR;
+
+    static {
+        String basedir;
+        if (SystemUtils.IS_OS_WINDOWS) {
+            // %LOCALAPPDATA%/APP_NAME/
+            // Fallback to %APPDATA%/APP_NAME/
+            String appdata = System.getenv("LOCALAPPDATA");
+            if (appdata == null || appdata.isEmpty()) {
+                appdata = System.getenv("APPDATA");
+            }
+            basedir = appdata;
+        } else if (SystemUtils.IS_OS_LINUX) {
+            // /home/x/.config/APP_NAME/
+            basedir = SystemUtils.USER_HOME + File.separator
+                + ".config" + File.separator;
+        } else {
+            // user.home/APP_NAME/
+            basedir = SystemUtils.USER_HOME;
+        }
+        CONFIG_DIR = basedir + File.separator +
+            APP_NAME + File.separator;
+
+        // Make config directory
+        File directory = new File(CONFIG_DIR);
+        directory.mkdirs();
+    }
+}
