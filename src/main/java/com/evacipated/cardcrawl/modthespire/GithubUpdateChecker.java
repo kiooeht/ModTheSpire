2a653f8df95d672ee4c1ba0ab786987289ce2205
Kiooeht <kiooeht@gmail.com>
2018-08-05 15:29:35 -0700

    Fallback on pre-releases if no latest release exists

--- src/main/java/com/evacipated/cardcrawl/modthespire/GithubUpdateChecker.java
+++ src/main/java/com/evacipated/cardcrawl/modthespire/GithubUpdateChecker.java
@@ -1,10 +1,12 @@
 package com.evacipated.cardcrawl.modthespire;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
+import com.google.gson.*;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -18,6 +20,37 @@ public class GithubUpdateChecker extends UpdateChecker
     public GithubUpdateChecker(String url) throws MalformedURLException
     {
         super(url);
+    }
+
+    private static String removeLatestFromURL(String url)
+    {
+        if (url.endsWith("/latest")) {
+            return url.substring(0, url.length() - "/latest".length());
+        }
+        return url;
+    }
+
+    @Override
+    protected void obtainLatestRelease() throws IOException
+    {
+        // If no latest release exists because all releases are pre-releases, grab the latest pre-release
+        try {
+            super.obtainLatestRelease();
+        } catch (FileNotFoundException e) {
+            jsonURL = new URL(removeLatestFromURL(jsonURL.toString()));
+
+            HttpURLConnection request = (HttpURLConnection) jsonURL.openConnection();
+            request.connect();
+
+            try {
+                JsonParser jp = new JsonParser();
+                JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+                latest = root.getAsJsonArray().get(0).getAsJsonObject();
+            } catch (JsonSyntaxException e2) {
+                System.out.println(jsonURL);
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     @Override
