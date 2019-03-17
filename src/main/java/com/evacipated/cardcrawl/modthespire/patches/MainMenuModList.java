9bc02121a31f7bdb56b9b190cde28eda34cd796a
Kiooeht <kiooeht@gmail.com>
2019-03-16 23:38:35 -0700

    Main menu no longer lists all mods, instead just the number of mods

--- src/main/java/com/evacipated/cardcrawl/modthespire/patches/MainMenuModList.java
+++ src/main/java/com/evacipated/cardcrawl/modthespire/patches/MainMenuModList.java
@@ -1,45 +1,58 @@
 package com.evacipated.cardcrawl.modthespire.patches;
 
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.evacipated.cardcrawl.modthespire.Loader;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
-import com.evacipated.cardcrawl.modthespire.patches.modsscreen.ModsScreen;
-import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.helpers.FontHelper;
 import com.megacrit.cardcrawl.screens.mainMenu.MainMenuScreen;
+import javassist.CannotCompileException;
+import javassist.expr.ExprEditor;
+import javassist.expr.FieldAccess;
+import javassist.expr.MethodCall;
 
 @SpirePatch(
     clz=MainMenuScreen.class,
     method="render"
 )
-public class MainMenuModList {
-    public static void Postfix(MainMenuScreen __instance, Object sb)
+public class MainMenuModList
+{
+    public static ExprEditor Instrument()
     {
-        if (__instance.screen != ModsScreen.Enum.MODS_LIST) {
-            float tmpy = 30.0F;
-            for (int i = Loader.MODINFOS.length - 1; i >= 0; --i) {
-                FontHelper.renderFontRightTopAligned(
-                    (SpriteBatch) sb,
-                    FontHelper.cardDescFont_N,
-                    Loader.MODINFOS[i].Name,
-                    Settings.WIDTH - 16.0F * Settings.scale + 200.0F * __instance.bg.slider,
-                    tmpy * Settings.scale,
-                    new Color(1.0F, 1.0F, 1.0F, 0.3F)
-                );
-                tmpy += 30.0F;
+        return new ExprEditor() {
+            @Override
+            public void edit(FieldAccess f) throws CannotCompileException
+            {
+                if (f.getFieldName().equals("VERSION_INFO")) {
+                    f.replace(String.format("$_ = %s.alterVersion($proceed($$));", MainMenuModList.class.getName()));
+                }
             }
 
-            // Mod(s): label
-            Color gold = Settings.GOLD_COLOR.cpy();
-            gold.a = 0.3F;
-            FontHelper.renderFontRightTopAligned(
-                (SpriteBatch) sb,
-                FontHelper.cardDescFont_N,
-                "Mod" + (Loader.MODINFOS.length > 1 ? "s" : "") + ":",
-                Settings.WIDTH - 16.0F * Settings.scale + 200.0F * __instance.bg.slider,
-                tmpy * Settings.scale,
-                gold);
-        }
+            @Override
+            public void edit(MethodCall m) throws CannotCompileException
+            {
+                if (m.getMethodName().equals("renderSmartText")) {
+                    m.replace(
+                        "{" +
+                            String.format("$5 = %s.getSmartHeight($2, $3, $6, $7);", MainMenuModList.class.getName()) +
+                            "$proceed($$);" +
+                            "}"
+                    );
+                }
+            }
+        };
+    }
+
+    public static String alterVersion(String version)
+    {
+        String ver = CardCrawlGame.VERSION_NUM;
+        String mtsver = ver.substring(ver.indexOf("ModTheSpire"), ver.length()-1);
+        ver = ver.substring(0, ver.indexOf(" [ModTheSpire"));
+        return MainMenuScreen.TEXT[0] + " NL " + ver + " NL " + mtsver + " - " + Loader.MODINFOS.length + " mod" + (Loader.MODINFOS.length > 1 ? "s" : "");
+    }
+
+    public static float getSmartHeight(BitmapFont font, String msg, float lineWidth, float lineSpacing)
+    {
+        return -FontHelper.getSmartHeight(font, msg, lineWidth, lineSpacing) + lineSpacing;
     }
 }
