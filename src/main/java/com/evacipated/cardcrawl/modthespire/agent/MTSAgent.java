d0acd01744c281a626b029749babaff990a09c63
Kiooeht <kiooeht@gmail.com>
2024-02-20 22:30:08 -0800

    Add agent transformer to "fix" WorkshopInfo.id now being a long instead of String
    In case mods were use it (ie. Texture Replacer)

--- src/main/java/com/evacipated/cardcrawl/modthespire/agent/MTSAgent.java
+++ src/main/java/com/evacipated/cardcrawl/modthespire/agent/MTSAgent.java
@@ -2,15 +2,18 @@ package com.evacipated.cardcrawl.modthespire.agent;
 
 import com.evacipated.cardcrawl.modthespire.Loader;
 import com.evacipated.cardcrawl.modthespire.ModTheSpire;
+import com.evacipated.cardcrawl.modthespire.steam.SteamSearch;
+import javassist.CannotCompileException;
 import javassist.ClassMap;
-import javassist.bytecode.ClassFile;
-import javassist.bytecode.Descriptor;
+import javassist.bytecode.*;
 
 import java.io.*;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.lang.instrument.Instrumentation;
 import java.security.ProtectionDomain;
+import java.util.List;
+import java.util.Objects;
 
 class MTSAgent
 {
@@ -18,6 +21,7 @@ class MTSAgent
     {
         System.out.println("ModTheSpire Premain");
         inst.addTransformer(new RenameLoader());
+        inst.addTransformer(new WorkshopInfoIDFixer());
         ModTheSpire.AGENT_ENABLED = true;
     }
 
@@ -29,6 +33,7 @@ class MTSAgent
         }
         System.out.println("Done.");
         inst.addTransformer(new RenameLoader());
+        inst.addTransformer(new WorkshopInfoIDFixer());
         ModTheSpire.AGENT_ENABLED = true;
     }
 
@@ -71,6 +76,90 @@ class MTSAgent
                     return barr.toByteArray();
                 }
             } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+            return null;
+        }
+    }
+
+    static class WorkshopInfoIDFixer implements ClassFileTransformer
+    {
+        @Override
+        public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException
+        {
+            try (DataInputStream in = new DataInputStream(new ByteArrayInputStream(classfileBuffer))) {
+                ClassFile cf = new ClassFile(in);
+                ClassMap cm = new ClassMap() {
+                    @Override
+                    public void put(String oldname, String newname)
+                    {
+                        put0(oldname, newname);
+                    }
+
+                    @Override
+                    public Object get(Object jvmClassName)
+                    {
+                        String n = toJavaName((String) jvmClassName);
+                        put0(n, n);
+                        return null;
+                    }
+
+                    @Override
+                    public void fix(String name) {}
+                };
+                cf.getRefClasses(cm);
+                if (cm.containsKey(SteamSearch.WorkshopInfo.class.getName())) {
+                    boolean fixedClass = false;
+                    ConstPool cp = cf.getConstPool();
+                    for (MethodInfo methodInfo : (List<MethodInfo>) cf.getMethods()) {
+                        boolean fixedMethod = false;
+                        CodeAttribute codeAttr = methodInfo.getCodeAttribute();
+                        if (codeAttr == null) {
+                            continue;
+                        }
+                        CodeIterator it = codeAttr.iterator();
+                        while (it.hasNext()) {
+                            try {
+                                int pos = it.next();
+
+                                int c = it.byteAt(pos);
+                                if (c == Opcode.INVOKEVIRTUAL) {
+                                    int v = it.u16bitAt(pos + 1);
+                                    String methodClassName = cp.getMethodrefClassName(v);
+                                    String methodName = cp.getMethodrefName(v);
+                                    String methodType = cp.getMethodrefType(v);
+                                    if (Objects.equals(methodClassName, SteamSearch.WorkshopInfo.class.getName()) && Objects.equals(methodName, "getID") && Objects.equals(methodType, "()Ljava/lang/String;")) {
+                                        Bytecode b = new Bytecode(cp);
+                                        // Replace "String getID()" calls with "long getID()"
+                                        b.addInvokevirtual(methodClassName, methodName, "()J");
+                                        // Then add "Long.toString(getID())" to mod receives a String as expected
+                                        b.addInvokestatic(Long.class.getName(), "toString", "(J)Ljava/lang/String;");
+                                        it.writeByte(Opcode.NOP, pos);
+                                        it.write16bit(Opcode.NOP, pos + 1);
+                                        it.insert(pos, b.get());
+                                        fixedMethod = true;
+                                    }
+                                }
+                            } catch (BadBytecode e) {
+                                throw new CannotCompileException(e);
+                            }
+                        }
+
+                        if (fixedMethod) {
+                            fixedClass = true;
+                        }
+                    }
+
+                    if (fixedClass) {
+                        ByteArrayOutputStream barr = new ByteArrayOutputStream();
+                        DataOutputStream out = new DataOutputStream(barr);
+                        cf.write(out);
+                        out.close();
+                        return barr.toByteArray();
+                    }
+                }
+            } catch (IOException | CannotCompileException e) {
                 e.printStackTrace();
                 return null;
             }
